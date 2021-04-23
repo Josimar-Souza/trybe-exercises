f40b9127216b4b1a082749a839b119ce3f473b91
Josimar Souza <contact.josimarsouza@gmail.com>
2021-04-23 10:02:53 -0300

    Resolvido a questão 3 do exercicio do dia 4.1

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercise-3.js
@@ -0,0 +1,11 @@
+let a = 15;
+let b = 18;
+let c = 27;
+
+if (a > b && a > c){
+    console.log(a + " é maior que " + b + " e " + c);
+}else if(b > a && b > c){
+    console.log(b + " é maior que " + a + " e " + c)
+}else{
+    console.log(c + " é maior que " + a + " e " + b)
+}
