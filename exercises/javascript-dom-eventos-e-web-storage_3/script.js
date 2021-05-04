d7e97ec52cdb57e4af92c43b1dce7a10cca65e2c
Josimar Souza <contact.josimarsouza@gmail.com>
2021-05-04 09:57:42 -0300

    Resolvido a primeira questao do exercicio do dia 5.3 e inicio d 2 questao

--- exercises/javascript-dom-eventos-e-web-storage_3/script.js
+++ exercises/javascript-dom-eventos-e-web-storage_3/script.js
@@ -14,3 +14,40 @@ function createDaysOfTheWeek() {
   createDaysOfTheWeek();
   
   // Escreva seu código abaixo.
+
+  generateMonthDays();
+  generateHolidayButton('Feriados');  
+  
+  function generateMonthDays() {
+    const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+    let daysList = document.getElementById('days');
+
+    for (let index = 0; index < dezDaysList.length; index += 1) {
+        let day = dezDaysList[index];
+        let dayItem = document.createElement('li');
+
+        if (day === 24 | day === 31) {
+            dayItem.className = 'day holiday';
+            dayItem.innerHTML = day;
+        } else if (day === 25) {
+            dayItem.className = 'day holiday friday';
+            dayItem.innerHTML = day;
+        } else if (day === 4 | day === 11 | day === 18) {
+            dayItem.className = 'day friday';
+            dayItem.innerHTML = day;    
+        } else {
+            dayItem.className = 'day';
+            dayItem.innerHTML = day;
+        };
+        daysList.appendChild(dayItem);
+    };
+  };
+
+  function generateHolidayButton(buttonName) {
+    let buttonsContainer = document.getElementsByClassName('buttons-container');
+    let button = document.createElement('button');
+    button.id = 'btn-holiday';
+    button.innerHTML = buttonName;
+    buttonsContainer[0].appendChild(button);    
+  };
