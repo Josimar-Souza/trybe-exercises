eefefc75109368accec44d03cbf34d946ca5886d
Josimar Souza <contact.josimarsouza@gmail.com>
2021-05-04 11:08:02 -0300

    Resolvido a questao 9 do exercicio do dia 5.3

--- exercises/javascript-dom-eventos-e-web-storage_3/script.js
+++ exercises/javascript-dom-eventos-e-web-storage_3/script.js
@@ -24,6 +24,7 @@ function createDaysOfTheWeek() {
   dayZoon();
   taskCreator('Cozinhar');
   taskSubtittle('red');
+  taskSelected();
   
   function generateMonthDays() {
     const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
@@ -135,8 +136,17 @@ function createDaysOfTheWeek() {
 
   function taskSelected() {
     let taskDivs = document.getElementsByClassName('task');
+    let selected = false;
 
     for (let task of taskDivs) {
-        console.log(task);
+        task.addEventListener('click', function() {
+            selected = !selected;
+
+            if(selected) {
+                task.className = 'task-selected';
+            } else {
+                task.className = 'task';
+            }
+        });
     };
   };
