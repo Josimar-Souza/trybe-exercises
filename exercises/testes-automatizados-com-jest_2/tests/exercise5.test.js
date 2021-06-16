5cc8b1c1e47f80fcae03b0fdff726a79e37e1de9
Josimar Souza <contact.josimarsouza@gmail.com>
2021-06-16 11:19:32 -0300

    Correçao no requisito 5

--- exercises/testes-automatizados-com-jest_2/tests/exercise5.test.js
+++ exercises/testes-automatizados-com-jest_2/tests/exercise5.test.js
@@ -13,7 +13,9 @@ describe('Scoped / Nested block', () => {
 // A saida será:
 // 1 - beforeEach
 // 1 - test
-// 1 -- afterEach
+// 1 - afterEach
+// 1 - beforeEach
 // 2 - beforeEach
 // 2 - test
 // 2 - afterEach
+// 1 - afterEach
