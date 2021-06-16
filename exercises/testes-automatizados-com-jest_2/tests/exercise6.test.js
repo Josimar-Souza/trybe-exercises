9acfb3e5beaa557dba13025cf8365f6474321dca
Josimar Souza <contact.josimarsouza@gmail.com>
2021-06-16 11:31:29 -0300

    Resolvido o requisito 6

---
+++ exercises/testes-automatizados-com-jest_2/tests/exercise6.test.js
@@ -0,0 +1,44 @@
+const Animals = [
+  { name: 'Dorminhoco', age: 1, type: 'Dog' },
+  { name: 'Soneca', age: 2, type: 'Dog' },
+  { name: 'Preguiça', age: 5, type: 'Cat' },
+];
+
+const findAnimalByName = (name) => {
+  // Adicione o código aqui.
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const list = Animals.filter((animal) => animal.name === name);
+      if (list.length !== 0) {
+        resolve(list[0]);
+      }
+      reject('Nenhum animal com esse nome!');
+    }, 100);
+  });
+};
+
+const getAnimal = (name) => {
+  // Adicione o código aqui.
+  return findAnimalByName(name).then((animal) => animal);
+};
+// ---------------------
+
+describe('Testando promise - findAnimalByName', () => {
+  describe('Quando existe o animal com o nome procurado', () => {
+    test('Retorne o objeto do animal', () => {
+      expect.assertions(1);
+      return getAnimal('Dorminhoco').then(animal => {
+        expect(animal).toEqual({ name: 'Dorminhoco', age: 1, type: 'Dog' });
+      });
+    });
+  });
+
+  describe('Quando não existe o animal com o nome procurado', () => {
+    test('Retorna um erro', () => {
+      expect.assertions(1);
+      return getAnimal('Bob').catch(error =>
+        expect(error).toEqual('Nenhum animal com esse nome!')
+      );
+    });
+  });
+});
