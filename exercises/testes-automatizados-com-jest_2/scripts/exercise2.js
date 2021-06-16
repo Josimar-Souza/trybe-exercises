9c3d734dc465747441d5368b74dae872f7a6e86b
Josimar Souza <contact.josimarsouza@gmail.com>
2021-06-16 10:43:56 -0300

    Corrigido o arquivo exercise2 e seu teste

--- exercises/testes-automatizados-com-jest_2/scripts/exercise2.js
+++ exercises/testes-automatizados-com-jest_2/scripts/exercise2.js
@@ -2,7 +2,7 @@ const users = {
   4: { name: 'Mark' },
   5: { name: 'Paul' }
 };
-  
+
 const findUserById = (id) => {
   return new Promise((resolve, reject) => {
       if (users[id]) {
@@ -19,4 +19,4 @@ const getUserName = (userID) => {
 
 module.exports = {
   getUserName,
-};
+}; 
--- exercises/testes-automatizados-com-jest_2/tests/exercise2.test.js
+++ exercises/testes-automatizados-com-jest_2/tests/exercise2.test.js
@@ -1,17 +1,18 @@
 const { describe, it, expect } = require('@jest/globals');
 const { getUserName } = require('../scripts/exercise2');
 
-describe('Testa a função getUserName', () => {
-  expect.assertions(2);
+describe('Testa a função getUserName', () => {  
   it('Testa caso o usuário seja encontrado', async () => {
+    expect.assertions(1);
     const userName = await getUserName(4);
     expect(userName).toBe('Mark');
   });
   it('Testa caso o usuário não seja encontrado', async () => {
+    expect.assertions(1);
     try{
-      getUserName(7);
-    } catch(error) {
-      expect(error).toEqual({ error: 'User with ' + id + ' not found.' });
+      await getUserName(7);
+    } catch (error) {
+      expect(error).toEqual({ error: 'User with 7 not found.' });
     }
   });
-});
+}); 
